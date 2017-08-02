7b8cfc1c07e68bc8cd48daca048f615b32d8d8fd
Sobey <David.Sobey@standardbank.co.za>
2017-08-02 19:50:47 +0200

    --

---
+++ CIMS/App_Start/BundleConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace CIMS
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/bower_components/jquery/jquery.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/bower_components/bootstrap/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrapCSS").Include(
+                      "~/bower_components/tether/tether.css",
+                      "~/bower_components/bootstrap/.css",
+                      "~/bower_components/font-awesome/font-awesome.css"
+                      ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/site.css"));
+        }
+    }
+}
