e8c409877c906b0a143754b604bfdb1667853b9a
David Sobey <david.sobey@standardbank.co.za>
2017-09-11 10:22:39 +0200

    Gitlab commit

--- CIMS/Controllers/HomeController.cs
+++ CIMS/Controllers/HomeController.cs
@@ -1,18 +1,49 @@
 ﻿using CIMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace CIMS.Controllers
 {
     public class HomeController : Controller
     {
+        private CIMS_NEWEntities db = new CIMS_NEWEntities();
         [Authorize]
         public ActionResult Index()
         {
-            CustomRoleProvider test = new CustomRoleProvider();
-            string a = test.GetANumber(User.Identity.Name);
+            CustomRoleProvider CRP = new CustomRoleProvider();
+            string a = CRP.GetANumber(username: Request.LogonUserIdentity.Name);
+            string[] Roles = CRP.GetRolesForUser(a);
+            List<Role> RoleList = new List<Role>();
+            foreach (string R in Roles)
+            {
+                try
+                {
+                    RoleList.AddRange(db.Roles.Where(Ro => Ro.RoleName.Equals('R')).ToList());
+                }
+                catch(Exception)
+                {
+
+                }
+            }
+            return View(RoleList);
+        }
+
+        public ActionResult Auth()
+        {
             return View();
         }
-        
+
+        [HttpPost]
+        public ActionResult Auth(string username)
+        {
+            CustomRoleProvider CRP = new CustomRoleProvider();
+            CRP.GetRolesForUser(username);
+            return RedirectToAction("index");
+        }
+
+
         public ActionResult Switch()
         {
             return View();
